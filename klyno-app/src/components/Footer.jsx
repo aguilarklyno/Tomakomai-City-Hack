a7659fccfa4825e85ddb628c2a9b23df8673162b
aguilarklyno <aguilarklyno@gmail.com>
2022-12-01 01:21:54 +0900

    commit

--- klyno-app/src/components/Footer.jsx
+++ klyno-app/src/components/Footer.jsx
@@ -2,25 +2,27 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='flex flex-col justify-center items-center w-full h-36 bg-blue-400 bottom-0 absolute'>
-      <div className='flex items-center justify-center w-11/12'>
-        <div className='flex items-center justify-center m-1 w-1/5 py-2 bg-white'>
-          <p className='md:text-lg text-sm font-bold'>概要</p>
+    <>
+      <div className='flex flex-col justify-center items-center w-full h-36 bg-blue-400 bottom-0 '>
+        <div className='flex items-center justify-center w-11/12'>
+          <div className='flex items-center justify-center m-1 w-1/5 py-2 bg-white'>
+            <p className='md:text-lg text-sm font-bold'>概要</p>
+          </div>
+          <div className='flex items-center justify-center m-1 w-1/5 py-2 bg-white'>
+            <p className='md:text-lg text-sm font-bold'>概要</p>
+          </div>
+          <div className='flex items-center justify-center m-1 w-1/5 py-2 bg-white'>
+            <p className='md:text-lg text-sm font-bold'>概要</p>
+          </div>
+          <div className='flex items-center justify-center m-1 w-1/5 py-2 bg-white'>
+            <p className='md:text-lg text-sm font-bold'>概要</p>
+          </div>
         </div>
-        <div className='flex items-center justify-center m-1 w-1/5 py-2 bg-white'>
-          <p className='md:text-lg text-sm font-bold'>概要</p>
-        </div>
-        <div className='flex items-center justify-center m-1 w-1/5 py-2 bg-white'>
-          <p className='md:text-lg text-sm font-bold'>概要</p>
-        </div>
-        <div className='flex items-center justify-center m-1 w-1/5 py-2 bg-white'>
-          <p className='md:text-lg text-sm font-bold'>概要</p>
+        <div>
+          <p className='my-5 text-white'>@ TOMAKOMAI CITY HACH / Team -「勝ちます」</p>
         </div>
       </div>
-      <div>
-        <p className='my-5 text-white'>@ TOMAKOMAI CITY HACH / Team -「勝ちます」</p>
-      </div>
-    </div>
+    </>
   )
 }
 
